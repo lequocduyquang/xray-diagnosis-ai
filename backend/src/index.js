aec7a8d2d5369ef625f93e2602b5cea12c497181
lequocduyquang <duyquangbtx@gmail.com>
2025-05-10 11:47:17 +0700

    chore: enable cors

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,10 +1,14 @@
 import express from "express";
 import dotenv from "dotenv";
+import cors from "cors";
 import imageRoutes from "./routes/imageRoutes.js";
 
 dotenv.config();
 
 const app = express();
+
+app.use(cors());
+
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
