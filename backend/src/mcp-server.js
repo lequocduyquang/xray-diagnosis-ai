6377653259a1aa08bdd72d2df013b892307253f5
quanglequocduy <quangle@hipagesgroup.com.au>
2025-06-25 09:58:01 +0700

    chore: setup some stuffs with mcp

---
+++ backend/src/mcp-server.js
@@ -0,0 +1,23 @@
+import { McpServer } from "@modelcontextprotocol/sdk/server/mcp.js";
+import { StdioServerTransport } from "@modelcontextprotocol/sdk/server/stdio.js";
+import { z } from "zod";
+
+// Create an MCP server
+const server = new McpServer({
+  name: "Demo",
+  version: "1.0.0"
+});
+
+// Add an addition tool
+server.tool("add",
+  { 
+    a: z.number().describe("The first number to add"),
+    b: z.number().describe("The second number to add")
+  },
+  async ({ a, b }) => ({
+    content: [{ type: "text", text: String(a + b) }]
+  })
+);
+// Start receiving messages on stdin and sending messages on stdout
+const transport = new StdioServerTransport();
+await server.connect(transport);
